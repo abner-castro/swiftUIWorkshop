ef4bd1ccf9dc5abac1505a0578d05ec47f6fd866
abner-castro <abner.castro@wizeline.com>
2022-05-11 17:15:40 -0500

    Initial Commit

---
+++ SwiftUIWorkshops/SwiftUIWorkshopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWorkshopsApp.swift
+//  SwiftUIWorkshops
+//
+//  Created by Abner Castro on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWorkshopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
